5d997d6cc47f4f49157c23e7c675bdd309e8d15f
Jess Doyle <jessica.e.doyle@gmail.com>
2019-07-09 13:18:05 -0400

    AVG

---
+++ average.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function average (numbers) {
+
+  let sum = 0;
+  numbers.forEach(function(element) {sum = sum + element});
+
+  return sum/numbers.length;
+
+
+}
+
+
+console.log(average([1, 2, 3, 4, 5, 6]));
+console.log(average([10, 10, 10]));
+console.log(average([-1, 1, -2, -5]));
+console.log(average([]));
