4fa9520a7080442763fbe4b47ad648f1bb3d50c0
Ahmed <ahmed.max7586@gmail.com>
2019-07-09 12:31:00 -0400

    created function min

--- max_and_min.js
+++ max_and_min.js
@@ -17,6 +17,24 @@ function max (numbers) {
 
 }
 
-console.log(max([1, 3, 7, 4, 6, 5]));
-console.log(max([]));
 
+function min (numbers) {
+
+  let min = numbers[0];
+  let i=0 ;
+  while (i<numbers.length){
+    if (numbers [i] < min ){
+      min = numbers[i] ;
+    }
+    i ++;
+  }
+  return min ;
+}
+
+
+
+// console.log(max([1, 3, 7, 4, 6, 5]));
+// console.log(max([]));
+
+console.log(min([1, 3, 7, 4, 6, 5]));
+console.log(min([]));
